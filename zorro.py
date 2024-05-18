a61fd66ad593aacd77bfbf02b7e93d92a7f29069
Oriol Borrajo <146335933+uriborrajo@users.noreply.github.com>
2024-05-18 20:24:59 +0200

    Update zorro.py

--- zorro.py
+++ zorro.py
@@ -2,32 +2,34 @@
 
 from Bio import AlignIO
 import glob
-import numpy
+import numpy as np
 
-threshold = 5 #float(raw_input('What HMM value do you which to exclude? '))
+threshold = 5
 
 fastas = glob.glob('*.fasta')
 
 for i in fastas:
-        handle = open(i, 'rb')
-        alignment = AlignIO.read(i, 'fasta')
-        align_array = numpy.array([list(rec) for rec in alignment], numpy.character, order="F")
-        OGname = i.rstrip('.fasta')
-        zorro_handle = open(OGname + '.fasta.mask', 'rb')
-        count = 0
-        cut = align_array[:,:1]
-        for j in zorro_handle:
-                if float(j.rstrip('\n')) >= threshold:
-                        cut = numpy.column_stack((cut, align_array[:,count]))
-                count += 1
-        cut = cut[:,1:]
-        handle.close()
-        zorro_handle.close()
+    handle = open(i, 'rb')
+    alignment = AlignIO.read(i, 'fasta')
+    align_array = np.array([list(rec) for rec in alignment], 'S1', order="F")
+    OGname = i.rstrip('.fasta')
+    zorro_handle = open(OGname + '.fasta.mask', 'rb')
+    count = 0
+    cut = align_array[:,:1]
+    for j_bytes in zorro_handle:
+        j_str = j_bytes.decode('utf-8') 
+        if float(j_str.rstrip('\n')) >= threshold:
+            cut = np.column_stack((cut, align_array[:, count]))
+        count += 1
+    cut = cut[:,1:]
+    handle.close()
+    zorro_handle.close()
 
-        outfile = open(i + '.cut', 'w')
-        for q in range(len(alignment)):
-                seq = ''
-                for p in cut[q,:]:
-                        seq += p
-                outfile.write('>' + alignment[q].id + '\n' + seq + '\n')
-        outfile.close()
+    outfile = open(i + '.cut', 'w')
+    for q in range(len(alignment)):
+        seq = ''
+        for p in cut[q,:]:
+            seq += p.decode('utf-8')  
+        outfile.write('>' + alignment[q].id + '\n' + seq + '\n')
+    outfile.close()
+
